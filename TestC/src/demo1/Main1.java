67a0e8e713439869ead25bc254ac6a159a7c964b
Maxim77553300 <81866299+Maxim77553300@users.noreply.github.com>
2021-11-01 19:04:25 +0300

    added library log4j and junit

--- TestC/src/demo1/Main1.java
+++ TestC/src/demo1/Main1.java
@@ -1,7 +1,11 @@
 package demo1;
 
+
+import org.apache.log4j.Logger;
+
 public class Main1 {
     public static void main(String[] args) {
+        Logger logger;
         Cat1 cat = new Cat1("Barsik");
         cat.printNameCat();
         System.out.println(cat.getKitten("").getName());
